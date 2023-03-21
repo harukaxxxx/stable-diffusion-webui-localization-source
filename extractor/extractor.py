b9377532a8c3f0c97cb38109cd67be3d482cb1e6
Rokurokubi <1701388+Rokurolize@users.noreply.github.com>
2023-03-22 03:44:48 +0900

    Added loading of existing data and sorting of JSON keys
    According to past commits, JSON was formatted, so the indent was changed to 2 accordingly.

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -22,9 +22,17 @@ for i in source_files:
     for key in list(source_data.keys()):
         if key in base_data:
             del source_data[key]
+
+    # if an output file with the same name as the source file exists, add keys from the existing file to the source file if they don't already exist in the source file
+    output_file_path = './source/extensions/' + os.path.basename(source_file_path)
+    if os.path.exists(output_file_path):
+        with open(output_file_path, 'r', encoding='utf-8') as f:
+            existing_data = json.load(f)
+        for key, value in existing_data.items():
+            if key not in source_data:
+                source_data[key] = value
+
     # save result to file
-        output_file_path = './source/extensions/' + \
-            os.path.basename(source_file_path)
-        with open(output_file_path, 'w', encoding='utf-8') as f:
-            json.dump(source_data, f, ensure_ascii=False, indent=4)
+    with open(output_file_path, 'w', encoding='utf-8') as f:
+        json.dump(source_data, f, ensure_ascii=False, indent=2, sort_keys=True)
     print(f'{output_file_path} extracted.')
