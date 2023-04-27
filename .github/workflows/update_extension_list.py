a18a8dfe891b8134970509efc5e62fbf2c2ff381
MakinoHaruka <harukaxxxx@makinoworks.com>
2023-04-27 17:52:25 +0000

    🐎 ci(issue_handler): create issue handler
    issue handler will auto create issue follow steaps below:
    1.scan 'source/extensions/' json files.
    2. get 'stable-diffusion-webui-extensions' repo extension list.
    3. get opened or closed  issuelist.
    4. compare 3 list based on step 2 list for missing extensions.
    5. create missing extensions issue.

--- .github/workflows/update_extension_list.py
+++ .github/workflows/update_extension_list.py
@@ -3,7 +3,7 @@ import urllib.request
 
 # gathering extension data from stable-diffusion-webui-extensions
 with urllib.request.urlopen('https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui-extensions/master/index.json') as response:
-    extension_data = json.loads(response.read().decode())['extensions']
+    extensions_data = json.loads(response.read().decode())['extensions']
 
 
 def extract_name_and_description(data, index):
@@ -12,12 +12,12 @@ def extract_name_and_description(data, index):
 
 
 # combine extracted data
-for index in range(0, len(extension_data)):
-    data = extract_name_and_description(extension_data, index)
-    if index == 0 and data:
-        extracted_data = data
-    elif data:
-        extracted_data.update(data)
+for index in range(0, len(extensions_data)):
+    extension_data = extract_name_and_description(extensions_data, index)
+    if index == 0 and extension_data:
+        extracted_data = extension_data
+    elif extension_data:
+        extracted_data.update(extension_data)
 
 # save to ExtensionList.json
 with open('./source/ExtensionList.json', 'w', encoding='utf-8') as outfile:
